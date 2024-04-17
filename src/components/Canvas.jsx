d61aa8cb8cd8bc9c0a39bd6f9d7e86183655ba8b
techmefr <compigni.gaetan@gmail.com>
2024-04-17 03:23:56 +0200

    add an useEffect to control windows size and listen to draw the canvas

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,20 +1,51 @@
-import { useEffect, useRef } from 'react';
-import PropTypes from 'prop-types';
+import { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
 
-import CanvasConfig from '../services/Canvas';
-import selectChart from '../services/charts/selectChart';
-import initAxies from '../services/initAxies';
+import CanvasConfig from "../services/Canvas";
+import selectChart from "../services/charts/selectChart";
+import initAxies from "../services/initAxies";
 
 const Canvas = ({ config }) => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    // create a new Class for setup the canvas
     const canvasCfg = new CanvasConfig(canvasRef, config.height, config.width);
-    // init the canvas using configuration of the class and props
     initAxies(canvasCfg, config);
-    // select the chart for the canvas
     selectChart(config, canvasCfg);
+  }, [config]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const currentWidth = canvasRef.current.offsetWidth;
+      const currentHeight = canvasRef.current.offsetHeight;
+      const originalWidth = config.width;
+      const originalHeight = config.height;
+      const widthDifference = Math.abs(originalWidth - currentWidth);
+      const heightDifference = Math.abs(originalHeight - currentHeight);
+      const widthChange = widthDifference / originalWidth >= 0.1;
+      const heightChange = heightDifference / originalHeight >= 0.1;
+
+      if (widthChange || heightChange) {
+        const newConfig = {
+          ...config,
+          width: currentWidth,
+          height: currentHeight,
+        };
+        const canvasCfg = new CanvasConfig(
+          canvasRef,
+          currentHeight,
+          currentWidth
+        );
+        initAxies(canvasCfg, newConfig);
+        selectChart(newConfig, canvasCfg);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, [config]);
 
   return <canvas ref={canvasRef} />;
