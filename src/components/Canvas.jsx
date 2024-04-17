d8cfbb8aeb108f22798529905ca10260ae954f9b
techmefr <compigni.gaetan@gmail.com>
2024-04-17 03:27:28 +0200

    do responsive canvas

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -16,29 +16,16 @@ const Canvas = ({ config }) => {
 
   useEffect(() => {
     const handleResize = () => {
-      const currentWidth = canvasRef.current.offsetWidth;
-      const currentHeight = canvasRef.current.offsetHeight;
-      const originalWidth = config.width;
-      const originalHeight = config.height;
-      const widthDifference = Math.abs(originalWidth - currentWidth);
-      const heightDifference = Math.abs(originalHeight - currentHeight);
-      const widthChange = widthDifference / originalWidth >= 0.1;
-      const heightChange = heightDifference / originalHeight >= 0.1;
-
-      if (widthChange || heightChange) {
-        const newConfig = {
-          ...config,
-          width: currentWidth,
-          height: currentHeight,
-        };
-        const canvasCfg = new CanvasConfig(
-          canvasRef,
-          currentHeight,
-          currentWidth
-        );
-        initAxies(canvasCfg, newConfig);
-        selectChart(newConfig, canvasCfg);
-      }
+      const { current: canvas } = canvasRef;
+      const canvasCfg = new CanvasConfig(
+        canvasRef,
+        window.innerHeight / 2,
+        window.innerWidth
+      );
+      canvas.width = canvasCfg.canvas.width;
+      canvas.height = canvasCfg.canvas.height;
+      initAxies(canvasCfg, config);
+      selectChart(config, canvasCfg);
     };
 
     window.addEventListener("resize", handleResize);
