3065b46d11c6c1a2e126525439792a09ce47acfa
Benjamin CHAILLAN <benjamin.chaillan-pro@laposte.net>
2024-04-24 11:55:49 +0200

    fix grid and x start on chart bar

--- src/services/charts/ChartBar.js
+++ src/services/charts/ChartBar.js
@@ -1,56 +1,52 @@
 import ChartPoint from './ChartPoint';
 
 class ChartBar extends ChartPoint {
-  constructor(data,context, spacing,barWidth = 200, fillColor = 'black', strokeColor = 'black',height = innerHeight / 2,width = innerWidth,cfgGrid = false) {
-    super( data,context,spacing,2,fillColor,strokeColor,height,width,false,cfgGrid);
+  constructor(
+    data,
+    context,
+    spacing,
+    barWidth = 200,
+    fillColor = 'black',
+    strokeColor = 'black',
+    height = innerHeight / 2,
+    width = innerWidth,
+    cfgGrid = false
+  ) {
+    super(
+      data,
+      context,
+      spacing,
+      2,
+      fillColor,
+      strokeColor,
+      height,
+      width,
+      false,
+      cfgGrid
+    );
     this.barWidth = barWidth;
-    this.startX = this.spacing;
   }
 
   drawBarArray() {
-    // if (this.cfgGrid === true) {
-    //   this.drawGrid();
-    // }// init value of row and column for draw again
-this.initStartForClmnAndRow();
-this.drawGrid();
-
-    this.initStartX();
+    //Init grid
+    this.drawGrid();
+    // Init column and row after grid
+    this.initStartForClmnAndRow();
+    // Draw graph Bar
     this.drawBars();
-  }
-
-  drawGrid(data = this.data) {
-    for (let i = 0; i < data.length; i++) {
-      this.twoPoint.drawLine(
-        this.context,
-        this.startX,
-        this.height,
-        this.startX,
-        this.spacing,
-        'grey'
-      );
-      this.nextX();
-    }
   }
 
   drawBars(data = this.data) {
     for (let i = 0; i < data.length; i++) {
       this.context.fillStyle = this.fillColor;
       this.context.fillRect(
-        this.startX,
+        this.startColumn + this.ratioW / 2 - this.barWidth / 2,
         this.height - data[i] * this.scaleH,
         this.barWidth,
         data[i] * this.scaleH
       );
-      this.nextX();
+      this.nextColumnAndRow();
     }
-  }
-
-  nextX() {
-    this.startX += this.ratioW;
-  }
-
-  initStartX() {
-    this.startX = this.spacing;
   }
 }
 
