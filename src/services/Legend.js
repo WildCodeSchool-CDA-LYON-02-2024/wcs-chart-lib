048a0ba4283949b6c6d804d48df239f58e5a4da5
techmefr <compigni.gaetan@gmail.com>
2024-04-24 16:31:54 +0200

    finish legend on the top

--- src/services/Legend.js
+++ src/services/Legend.js
@@ -12,10 +12,10 @@ class Legend {
       case "inline":
         this.drawInlineLegend(context, canvas, spacing);
         break;
-      case 'blockLeft':
+      case "blockLeft":
         this.drawBlockLeftLegend(context, canvas, spacing);
         break;
-      case 'blockRight':
+      case "blockRight":
         this.drawBlockRightLegend(context, canvas, spacing);
         break;
       default:
@@ -23,23 +23,23 @@ class Legend {
     }
   }
   // TODO : changer légende en fonction du style de graphique (actuellement adapté au camembert, mais quand une ligne il ne faut qu'un titre, dans les barres il faut les couleurs avec les labels)
-
   drawLegendOnTheTop(context, canvas, spacing) {
     const legendHeight = spacing;
     const legendWidth = canvas.width;
     const legendX = 0;
     const legendY = 0;
 
-    context.fillStyle = "#dddddd";
+    context.fillStyle = "transparent";
     context.fillRect(legendX, legendY, legendWidth, legendHeight);
 
     context.fillStyle = "black";
     context.font = "bold 24px Roboto";
     context.textAlign = "center";
-    context.fillText("Legend", canvas.width / 2, 25);
+    context.fillText("Légende", canvas.width / 2, 20);
 
-    const labelSpacing = 20;
-    let yPosition = 30;
+    const labelSpacing = 120;
+    let totalLabelsWidth = this.labels.length * labelSpacing - 10;
+    let startX = (canvas.width - totalLabelsWidth) / 2;
 
     for (let i = 0; i < this.labels.length; i++) {
       const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
@@ -47,13 +47,13 @@ class Legend {
       )}, ${Math.floor(Math.random() * 256)})`;
 
       context.fillStyle = randomColor;
-      context.fillRect(legendX + 2, yPosition, 10, 10);
+      context.fillRect(startX, 40, 10, 10);
 
       context.fillStyle = "black";
       context.font = "14px Roboto";
-      context.fillText(this.labels[i], legendX + 50, yPosition + 5);
+      context.fillText(this.labels[i], startX + 50, 50);
 
-      yPosition += labelSpacing;
+      startX += labelSpacing;
     }
   }
 
@@ -63,7 +63,7 @@ class Legend {
     const legendX = 0;
     const legendY = canvas.height - legendHeight;
 
-    context.fillStyle = '#dddddd';
+    context.fillStyle = "#dddddd";
     context.fillRect(legendX, legendY, legendWidth, legendHeight);
 
     context.fillStyle = "black";
@@ -74,7 +74,7 @@ class Legend {
 
   drawBlockLeftLegend(context, canvas, spacing) {
     const legendHeight = canvas.height;
-    const legendWidth = spacing;
+    const legendWidth = canvas.width;
     const legendX = 0;
     const legendY = 0;
 
@@ -99,7 +99,7 @@ class Legend {
 
       context.fillStyle = "black";
       context.font = "14px Roboto";
-      context.fillText(this.labels[i], legendX + 20, yPosition + 40);
+      context.fillText(this.labels[i], legendX, yPosition + 40);
 
       yPosition += labelSpacing;
     }
